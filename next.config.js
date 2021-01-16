5a34fdc08c1ea41bb7b7cc4e798e1354aaaa16e9
Andrew Hurle <fracture91@gmail.com>
2021-01-16 12:35:08 -0500

    Add @next/bundle-analyzer, run with `ANALYZE=true yarn build`

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   trailingSlash: true,
   webpack: function (config) {
     config.module.rules.push({
@@ -11,4 +15,4 @@ module.exports = {
     };
     return config;
   }
-};
+});
