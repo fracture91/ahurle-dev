38e1bb18db8ec250dbe2c1bef4cdcbcf0b39fd34
Andrew Hurle <fracture91@gmail.com>
2021-01-16 17:32:23 -0500

    Convert <a> tags with internal hrefs to next/link for client-side routing

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from "next/link";
 import { format } from 'fecha';
 import { PostData } from '../loader';
 import { Tag } from './Tag';
@@ -6,30 +7,32 @@ import { Tag } from './Tag';
 export const PostCard: React.FC<{ post: PostData }> = (props) => {
   const post = props.post;
   return (
-    <a className="post-card" href={`/${post.path}`}>
-      <div className="post-card-inner">
-        {post.thumbnailPhoto && (
-          <div
-            className="post-card-thumbnail"
-            style={{ backgroundImage: `url(${post.thumbnailPhoto})` }}
-          />
-        )}
-        <div className="post-card-title">
-          {post.title && <h2>{post.title}</h2>}
-          {false && post.subtitle && <p>{post.subtitle}</p>}
-          <p>
-            {props.post.datePublished
-              ? format(new Date(props.post.datePublished), 'MMMM Do, YYYY')
-              : ''}
-          </p>
-          <div className="flex-spacer"> </div>
-          {false && (
-            <div className="tag-container">
-              {post.tags && (post.tags || []).map((tag) => <Tag tag={tag} />)}
-            </div>
+    <Link href={`/${post.path}`}>
+      <a className="post-card">
+        <div className="post-card-inner">
+          {post.thumbnailPhoto && (
+            <div
+              className="post-card-thumbnail"
+              style={{ backgroundImage: `url(${post.thumbnailPhoto})` }}
+            />
           )}
+          <div className="post-card-title">
+            {post.title && <h2>{post.title}</h2>}
+            {false && post.subtitle && <p>{post.subtitle}</p>}
+            <p>
+              {props.post.datePublished
+                ? format(new Date(props.post.datePublished), 'MMMM Do, YYYY')
+                : ''}
+            </p>
+            <div className="flex-spacer"> </div>
+            {false && (
+              <div className="tag-container">
+                {post.tags && (post.tags || []).map((tag) => <Tag tag={tag} />)}
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-    </a>
+      </a>
+    </Link>
   );
 };
