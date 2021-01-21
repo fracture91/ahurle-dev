8bd9367b0264c9ebbc1f8e7d85da2fc0d2977ac0
Andrew Hurle <fracture91@gmail.com>
2021-01-21 13:04:08 -0500

    Use vercel preview URL outside of prod mode, localhost in dev mode

--- globals.ts
+++ globals.ts
@@ -1,3 +1,5 @@
+const prodUrl = 'https://ahurle.dev';
+
 export namespace globals {
   export const yourName = 'Andrew Hurle';
   export const siteName = 'ahurle.dev';
@@ -5,7 +7,10 @@ export namespace globals {
   export const siteCreationDate = 'January 15, 2021 20:00:00 GMT';
   // export const twitterHandle = '@';
   export const email = 'me@ahurle.dev';
-  export const url = 'https://ahurle.dev';
+  export const url =
+    process.env.VERCEL_ENV && process.env.VERCEL_ENV != "production" && process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : (process.env.NODE_ENV == "production" ? prodUrl : "http://localhost:3000");
   export const accentColor = '#4fc2b4';
   export const googleAnalyticsId = ``; // e.g. 'UA-999999999-1'
 }
