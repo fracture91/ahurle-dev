32dc89787ea2ba9e150c8bc82569b4a91ac5e1dc
Andrew Hurle <fracture91@gmail.com>
2021-01-22 22:34:29 -0500

    fix first test

--- __tests__/testing-library.tsx
+++ __tests__/testing-library.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { render } from "@testing-library/react"
 import Index from "../pages/index"
 
-test("renders deploy link", () => {
+test("renders text", () => {
   const { getByText } = render(<Index introduction="" features="" posts={[]} />)
   const linkElement = getByText(/Introduction to Devii/)
   expect(linkElement).toBeInTheDocument()
