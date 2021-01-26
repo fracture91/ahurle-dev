c99f38bfd88b6c0969a82da1a0d039d0c3d9eb5d
Andrew Hurle <fracture91@gmail.com>
2021-01-26 14:06:55 -0500

    Fix test failing due to "invalid hook call"
    Also make sure the custom document is getting rendered with useDocument
    
    Also I deduped the yarn.lock file

--- __tests__/pages/index.tsx
+++ __tests__/pages/index.tsx
@@ -3,7 +3,18 @@ import { screen } from "@testing-library/react"
 
 describe("Homepage", () => {
   it("renders nicely", async () => {
-    const { render } = await getPage({ route: "/" })
+    const { render } = await getPage({
+      route: "/",
+      useDocument: true,
+      // Without this, you'll get an "Invalid hook call" error for anything that uses hooks.
+      // This only started happening after applying the jest-runtime patch to fix styled-components.
+      // Unclear why the blog test doesn't have this problem.
+      // I tried all the recommended debugging steps and even trudged through the debugger
+      // trying to understand what's going on, but couldn't.
+      // Also tried creating a minimum reproducible test case from scratch but couldn't get the bug to happen!
+      // I could work backwards and strip things from this project until I have a test case but... this works
+      nonIsolatedModules: ["react"],
+    })
     render()
     expect(screen.getByText("ahurle.dev")).toBeVisible()
     expect(screen.getByText("Introduction to Devii")).toBeVisible()
