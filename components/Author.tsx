95fb50cd0db01103c0ddd706f7ec83c286ae4ab0
Andrew Hurle <fracture91@gmail.com>
2021-02-25 22:16:48 -0500

    Less-janky AuthorLines separators

--- components/Author.tsx
+++ components/Author.tsx
@@ -6,20 +6,36 @@ import Image from "next/image"
 import type * as readingTime from "reading-time"
 import { WrapFC } from "@/helpers/WrapFC"
 
-const separatorSizeEms = 0.9
-
+/**
+ * Takes advantage of whitespace collapsing to show separators only when two
+ * things are on the same line.
+ */
 const Separated: WrapFC<typeof Text> = (props) => (
   <Text
     {...props}
     sx={{
-      ml: "-0.6em",
-      mr: `${separatorSizeEms}em`,
-      "&:before": { content: "'· '" },
+      whiteSpace: "nowrap",
+      display: "inline",
+      "&:after": {
+        content: "' '",
+        whiteSpace: "normal",
+        letterSpacing: "0.75em",
+        background: `radial-gradient(
+            circle at 50%,
+            currentColor 0.08em,
+            transparent 0.12em
+          )`,
+      },
     }}
   />
 )
 
-const containsSeparated = { mr: `${-separatorSizeEms}em` }
+const groupSpacing = ["1em", "1.5em", "3em"]
+const negGroupSpacing = groupSpacing.map((s) => `-${s}`)
+
+const Group: WrapFC<"p"> = (props) => (
+  <p {...props} sx={{ m: "2px", mr: groupSpacing, display: "inline-block" }} />
+)
 
 const dateFormatter = new Intl.DateTimeFormat("en-US", {
   year: "numeric",
@@ -31,39 +47,30 @@ export const AuthorLines: React.FC<{
   post: BlogMeta
   readingTime: ReturnType<typeof readingTime.default>
 }> = ({ post, readingTime }) => (
-  <div sx={{ lineHeight: 1.2, overflow: "hidden", flexGrow: 1 }}>
-    <p sx={{ m: "2px", ...containsSeparated }}>
+  <div sx={{ lineHeight: 1.2, flexGrow: 1, mr: negGroupSpacing }}>
+    <Group>
       {post.author?.name && (
-        <Separated sx={{ color: "primary", whiteSpace: "nowrap" }}>
-          {post.author?.name}
-        </Separated>
+        <Separated sx={{ color: "primary" }}>{post.author?.name}</Separated>
       )}
 
       {post.author?.twitter && (
-        <Separated sx={{ display: "inline-block" }}>
-          <a
-            href={`https://twitter.com/${post.author?.twitter}`}
-          >{`@${post.author?.twitter}`}</a>{" "}
+        <Separated>
+          <a href={`https://twitter.com/${post.author?.twitter}`}>
+            {`@${post.author?.twitter}`}
+          </a>
         </Separated>
       )}
-    </p>
-    <p
-      sx={{
-        color: "tertiary",
-        opacity: 0.8,
-        m: "2px",
-        ...containsSeparated,
-      }}
-    >
+    </Group>
+    <Group sx={{ color: "tertiary", opacity: 0.8 }}>
       {post.datePublished && (
         <>
-          <Separated sx={{ display: "inline-block" }}>
+          <Separated>
             {dateFormatter.format(new Date(post.datePublished))}
           </Separated>
         </>
       )}
-      <Separated sx={{ display: "inline-block" }}>{readingTime.text}</Separated>
-    </p>
+      <Separated>{readingTime.text}</Separated>
+    </Group>
   </div>
 )
 
@@ -82,7 +89,7 @@ export const Author: React.FC<{
     }}
   >
     {post.author?.photo && (
-      <Box mr={2} sx={{ lineHeight: 0, flexShrink: 0 }}>
+      <Box mr={[2, null, 3]} sx={{ lineHeight: 0, flexShrink: 0 }}>
         <Image
           src={post.author.photo.src}
           alt={post.author.photo.alt}
