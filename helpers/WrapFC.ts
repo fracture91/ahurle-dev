42f5fc9f1735d896e870d8d7115e86f53f4caf0a
Andrew Hurle <fracture91@gmail.com>
2021-03-08 16:18:34 -0500

    Let WrapFC consume additional props

--- helpers/WrapFC.ts
+++ helpers/WrapFC.ts
@@ -1,5 +1,13 @@
 import React, { ElementType } from "react"
 
-export type WrapFC<T extends ElementType> = React.FC<
-  React.ComponentPropsWithRef<T>
+export type WrapFC<
+  T extends ElementType,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  U extends Record<string, unknown> = {},
+  V extends string | undefined = undefined
+> = React.FC<
+  (V extends string
+    ? Omit<React.ComponentPropsWithRef<T>, V>
+    : React.ComponentPropsWithRef<T>) &
+    U
 >
