a3f05a6f771afd866767d0dfc887a701a7fca7d6
Andrew Hurle <fracture91@gmail.com>
2021-04-04 12:59:03 -0400

    Center logo SVG, add color, link as favicon, generate ICO file

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,10 +4,10 @@ import { WrapFC } from "@/helpers/WrapFC"
 
 // me being lazy and not wanting to set up a webpack loader
 export const Logo: WrapFC<"svg"> = ({ ...props }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 139 129" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 139 139" {...props}>
     <path
       fill="currentColor"
-      d="M38 0v54L0 129h16l22-43v43h10V75h43v54h11V86l21 43h16l-37-75V0H91v32L75 0H64L48 32V0H38zm32 21l21 43H48l22-43z"
+      d="M38 5v54L0 134h16l22-43v43h10V80h43v54h11V91l21 43h16l-37-75V5H91v33L75 5H64L48 38V5H38zm32 22l21 43H48l22-43z"
     />
   </svg>
 )
