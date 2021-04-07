5ddaa34884dd2cdeddd7954f081ecff23564c2fb
Andrew Hurle <fracture91@gmail.com>
2021-04-07 14:39:01 -0400

    WIP: Fix GC double pageviews and many other problems

--- components/GoatCounter.tsx
+++ components/GoatCounter.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from "react"
+import React, { useEffect } from "react"
 import * as globals from "@/helpers/globals"
 import { useRouter } from "next/router"
 
@@ -19,13 +19,22 @@ declare global {
     // incomplete definition
     goatcounter: {
       count(vars: CountVars): void
+      filter(): string | false
       // eslint-disable-next-line camelcase
       bind_events(): void
       // eslint-disable-next-line camelcase
       no_onload: boolean
       // eslint-disable-next-line camelcase
       no_events: boolean
+      // provided by me, not GC, because I need a way to disable outside of the no_* props
+      disabled: boolean
+      // provided by me, not GC, so I can call `count` before the script loads
+      queue: (CountVars | undefined)[]
     }
+    // provided by me, so I can tell if the GC script loaded by comparing function ref
+    gcCountStub: typeof window.goatcounter.count
+    gcNoop: typeof window.goatcounter.count
+    handleGcScriptLoad(): void
   }
 }
 
@@ -47,7 +56,7 @@ const RealPixel: React.FC = () => {
     globals.goatCounterId === "ahurle-dev" &&
     (globals.siteName !== "ahurle.dev" ||
       // Skip analytics for all the staging vercel.app domains
-      (process.env.VERCEL_URL && process.env.VERCEL_URL !== "ahurle.dev"))
+      process.env.VERCEL_URL !== "ahurle.dev")
   )
     return null
 
@@ -67,66 +76,151 @@ export const GoatCounterPixel: React.FC = globals.goatCounterId
 
 // always include this JS inline so that I can call these functions without errors
 // in the event that the gc script doesn't load, or I want to quickly disable it
-const fallbackJs =
-  "window.goatcounter = window.goatcounter || { count: function(){}, bind_events: function(){} }"
+// the no_* stuff is because I need to trigger that stuff manually
+// In case the script finishes loading after my react stuff, queue up count calls for later.
+// Skip analytics for all the staging vercel.app domains
+// Also, the prod domain is hardcoded here as a safeguard against someone forking my repo
+// and accidentally dumping their data into my account.
+// If I'm talking about you: please change `globals.goatCounterId` before altering the host below!
+const fallbackJs = `
+  window.gcNoop = function(){}
+  window.gcCountStub = function(vars){
+    if(!this.disabled) this.queue.push(vars)
+  }
+  window.goatcounter = window.goatcounter || {
+    count: window.gcCountStub,
+    filter: window.gcNoop,
+    bind_events: window.gcNoop,
+  }
+  window.goatcounter.queue = []
+  window.goatcounter.no_onload = true
+  window.goatcounter.no_events = true
 
-const RealScript: React.FC = () => {
-  const router = useRouter()
-
-  const onRouteChange = useCallback(() => {
-    // just in case the goatcounter script blew up
-    if (!window.goatcounter) return
-    // goatcounter does not track pushState changes automatically - do it myself
-    // @ts-ignore: count() param is not actually required, but all other calls should have {event: true}
-    if (!window.goatcounter.no_onload) window.goatcounter.count()
-    // Goatcounter only adds event listeners on load e.g. for tracking data-goatcounter-click.
-    // Re-add them after the page changes, when a bunch of stuff has definitely re-rendered.
-    // Goatcounter is smart enough to avoid adding duplicate listeners.
-    // Note that if a component using data-goatcounter-click re-renders at a different time,
-    // it will need to call this itself since the original DOM node could have been removed.
-    if (!window.goatcounter.no_events) window.goatcounter.bind_events()
-  }, [])
-
-  useEffect(() => {
-    router.events.on("routeChangeComplete", onRouteChange)
-    return () => {
-      router.events.off("routeChangeComplete", onRouteChange)
-    }
-  }, [router, onRouteChange])
-
-  return (
-    <>
-      <script
-        // Skip analytics for all the staging vercel.app domains
-        // Also, the prod domain is hardcoded here as a safeguard against someone forking my repo
-        // and accidentally dumping their data into my account.
-        // If I'm talking about you: please change `globals.goatCounterId` before altering the host below!
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{
-          __html: `
-            ${fallbackJs}
-            if (window.location.host !== 'ahurle.dev') {
-              window.goatcounter.no_onload = true
-              window.goatcounter.no_events = true
-            }
-          `,
-        }}
-      />
-      <script
-        data-goatcounter={endpoint}
-        // uncomment for testing out on localhost
-        // data-goatcounter-settings='{"allow_local": true}'
-        async
-        src="//gc.zgo.at/count.js"
-      />
-    </>
-  )
-}
+  if (window.location.host !== 'ahurle.dev') {
+    // window.goatcounter.disabled = true
+  }
+`.replace(/[ \t]+/g, " ")
 
 const FallbackScript: React.FC = () => (
   // eslint-disable-next-line react/no-danger
   <script dangerouslySetInnerHTML={{ __html: fallbackJs }} />
 )
+
+// when the script loads successfully, it should overwrite my stub function
+const scriptLoaded = (): boolean =>
+  ![window.gcCountStub, window.gcNoop].includes(window.goatcounter.count)
+
+// important: can be called twice
+if (typeof window !== "undefined")
+  window.handleGcScriptLoad = () => {
+    // make sure when `disabled == true` I can't call `count` - better than checking disabled manually
+    const originalFilter = window.goatcounter.filter
+    window.goatcounter.filter = function filter(...args) {
+      console.log("wrapped filter called")
+      return this.disabled
+        ? "disabled with window.goatcounter.disabled"
+        : originalFilter.call(this, ...args)
+    }
+
+    // if the count stub hasn't been replaced, the loop below might go infinitely?
+    if (!scriptLoaded()) return
+
+    // if `count` was called before the script could load, queue will contain events to send
+    const { queue } = window.goatcounter
+    if (queue.length > 0) console.log("queue not empty")
+    while (queue.length > 0) {
+      console.log("popping from queue")
+      window.goatcounter.count(queue.pop() as CountVars)
+    }
+
+    // call bind_events like would usually happen onload
+    if (!window.goatcounter.disabled) window.goatcounter.bind_events()
+  }
+
+const onRouteChange = () => {
+  console.log("onRouteChange")
+  if (window.goatcounter.disabled) return
+  // If the user has navigated to a different page before the queue could be emptied,
+  // we should clear it out so as not to be confusing.  It might look like `[undefined, undefined]`
+  // and thus log duplicate pageviews.  Better to just drop the earlier ones.
+  window.goatcounter.queue = []
+  // goatcounter does not track pushState changes automatically - do it myself
+  // count() param is not actually required, but all other calls should have {event: true}
+  window.goatcounter.count((undefined as unknown) as CountVars)
+  // Goatcounter only adds event listeners on load e.g. for tracking data-goatcounter-click.
+  // Re-add them after the page changes, when a bunch of stuff has definitely re-rendered.
+  // Goatcounter is smart enough to avoid adding duplicate listeners.
+  // Note that if a component using data-goatcounter-click re-renders at a different time,
+  // it will need to call this itself since the original DOM node could have been removed.
+  window.goatcounter.bind_events()
+}
+
+const loadTimeout = 120 * 1000
+
+const RealScript: React.FC = () => {
+  const router = useRouter()
+  // const { asPath, query } = router
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // If the script still hasn't finished loading after the timeout, make sure we're not infinitely
+      // queueing events (i.e. leaking memory) by replacing our countStub with a no-op.
+      if (!scriptLoaded()) {
+        window.goatcounter.count = window.gcNoop
+        window.goatcounter.queue = []
+      }
+    }, loadTimeout)
+    // If the script finished loading before window.handleGcScriptLoad could be defined,
+    // its onLoad will have no effect. In that case call it here, on mount.
+    // If it did not finish loading yet, this function is called twice, which it is designed to handle
+    window.handleGcScriptLoad()
+    // next.js has a problem where routeChangeComplete can be called on first page load
+    // https://github.com/vercel/next.js/issues/11639
+    // https://github.com/vercel/next.js/discussions/12306
+    // detect if this erroneous call is about to happen and skip logging the pageview if so,
+    // since it's going to happen later in the routeChangeComplete handler
+    // this is why I need to always set goatcounter.no_onload = true - sometimes I need to skip it
+    console.log("pageload", router, onRouteChange)
+    if (
+      !router.asPath.includes("?") ||
+      Object.keys(router.query).length !== 0
+    ) {
+      console.log("init pageview")
+      onRouteChange()
+    }
+    router.events.on("routeChangeComplete", onRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChange)
+      clearTimeout(timer)
+    }
+    // only run once
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <>
+      <FallbackScript />
+      {/* in case the script finishes loading after the useEffect fires, I need that onload handler */}
+      {/* using onLoad in JSX is unreliable since it needs react to hydrate first, hence this awfulness */}
+      <div
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: `
+        <script
+          data-goatcounter=${JSON.stringify(endpoint)}
+          ${
+            // uncomment for testing out on localhost
+            "" + "data-goatcounter-settings='{\"allow_local\": true}'"
+          }
+          async
+          src="https://gc.zgo.at/count.js"
+          onload="window.handleGcScriptLoad && window.handleGcScriptLoad()"
+        ></script>`.replace(/[ \t]+/g, " "),
+        }}
+      />
+    </>
+  )
+}
 
 export const GoatCounterScript: React.FC = globals.goatCounterId
   ? RealScript
