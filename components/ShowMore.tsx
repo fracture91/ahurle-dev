0b0b820a1e61cd6b60d369267da5ab43e4ca1b77
Andrew Hurle <fracture91@gmail.com>
2021-04-11 19:08:13 -0400

    Improve a11y in ShowMore with an invisible input over the button
    Previously, you couldn't even focus the button.  Adding tabIndex allows
    focus but pressing enter/space had no effect.  Consequently, now we focus
    the input itself and make it the same size as the button.  Clicks hit the
    input and CSS triggers the hover styling on the button.

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,3 +1,4 @@
+import { theme } from "@/helpers/theme"
 import { WrapFC } from "@/helpers/WrapFC"
 import { Button } from "theme-ui"
 import { DownCaret } from "./DownCaret"
@@ -6,10 +7,12 @@ export const ShowMoreButton: WrapFC<
   typeof Button,
   {
     moreVisibleSelector: string
+    focusBasisSelector?: string
     htmlFor?: string
   }
 > = ({
   moreVisibleSelector,
+  focusBasisSelector,
   children = (
     <>
       Show <span>More</span>
@@ -29,6 +32,18 @@ export const ShowMoreButton: WrapFC<
         "span + span": { display: "inline" },
         svg: { transform: "rotate(-180deg)" },
       },
+      ...(focusBasisSelector
+        ? {
+            [`${focusBasisSelector}:focus ~ &, ${focusBasisSelector}:hover ~ &`]: {
+              ...theme.buttons.primary["&:hover, &:focus"],
+            },
+            [`${focusBasisSelector}:focus-visible ~ &`]: {
+              outline: "5px auto Highlight", // moz-specific
+              // I really want the same prop twice, hence the leading space
+              " outline": "5px auto -webkit-focus-ring-color", // chrome/safari
+            },
+          }
+        : {}),
     }}
   >
     {children}
@@ -45,11 +60,29 @@ export const ShowMoreButton: WrapFC<
   </Button>
 )
 
-const whenChecked = "input[type='checkbox']:checked ~ &"
+const selectCheckbox = "input[type='checkbox']"
+const whenChecked = `${selectCheckbox}:checked ~ &`
 
+// https://www.sarasoueidan.com/blog/inclusively-hiding-and-styling-checkboxes-and-radio-buttons/#hiding-the-checkboxes-inclusively
+// a hidden checkbox is positioned on top of the ShowMoreButton for a11y
 export const ShowMore: React.FC<{ id: string }> = ({ id, children }) => (
-  <div sx={{ my: "1em" }}>
-    <input id={id} type="checkbox" sx={{ display: "none" }} />
+  <div sx={{ my: "1em", position: "relative" }}>
+    <input
+      id={id}
+      type="checkbox"
+      sx={{
+        position: "absolute",
+        bottom: "-5px",
+        left: "-5px",
+        right: "-5px",
+        display: "block",
+        width: "100%",
+        height: "2.6em",
+        opacity: 0.00001,
+        zIndex: 1,
+        cursor: theme.buttons.primary.cursor,
+      }}
+    />
     <div
       sx={{
         order: 1,
@@ -62,6 +95,7 @@ export const ShowMore: React.FC<{ id: string }> = ({ id, children }) => (
     </div>
     <ShowMoreButton
       moreVisibleSelector={whenChecked}
+      focusBasisSelector={selectCheckbox}
       as="label"
       htmlFor={id}
       data-goatcounter-click={id}
